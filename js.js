85962f424933ebbaf1bbcce7e526ba48b1faa383
yahooashfaq <yahooashfaq@gmail.com>
2021-05-01 05:51:59 +0500

    first commit by eaglesoft

---
+++ js.js
@@ -0,0 +1,22 @@
+//  www.eaglesoft.com.pk
+
+let outputScreen;
+function display(num) {
+    outputScreen = document.getElementById("output-screen");
+    outputScreen.value += num;
+}
+
+function calculate() {
+   try{ outputScreen.value = eval(outputScreen.value);}
+   catch (err) {
+       alert("Invalid equation!")
+   }
+}
+
+function allClear() {
+    outputScreen.value = "";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
